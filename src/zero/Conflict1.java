4850e8eb83a5ea549d5d36771f2097638f10ae45
elviraktmu <elviraktmu@mail.ru>
2022-06-22 17:40:14 +0600

    Conf

--- src/zero/Conflict1.java
+++ src/zero/Conflict1.java
@@ -2,6 +2,6 @@ package zero;
 
 public class Conflict1 {
     public static void main(String[] args) {
-        System.out.println("This is first conflict");
+        System.out.println("This is first conflictgit");
     }
 }
