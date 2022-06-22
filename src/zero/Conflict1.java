bc4851bda96497dfb5019b9a637dacb1491a8812
Daniyarov Asan <turduhodzhaev99@gmail.com>
2022-06-22 12:47:43 +0600

    statics

--- src/zero/Conflict1.java
+++ src/zero/Conflict1.java
@@ -1,7 +1,7 @@
 package zero;
 
 public class Conflict1 {
-    public static void main(String[] args) {
+    public statics void main(String[] args) {
         System.out.println("This is first conflict1");
         System.out.println("this is second conflict");
     }
