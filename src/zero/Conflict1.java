894717ddb96214187f958b35862c430b4528a061
Daniyarov Asan <turduhodzhaev99@gmail.com>
2022-06-22 17:02:45 +0600

    adasf

--- src/zero/Conflict1.java
+++ src/zero/Conflict1.java
@@ -4,6 +4,7 @@ public class Conflict1 {
     public static  void main(String[] args) {
         System.out.println("This is first conflict1");
         System.out.println("this is second conflict");
+        asam
 
 
     }
